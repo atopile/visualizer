f8efe2fce3374aabcb7cf6c44886f5e72d746fe8
Timot05 <timothee.peter@mac.com>
2024-04-11 14:13:32 -0700

    remove index

--- visualizer/src/CustomNode.tsx
+++ visualizer/src/CustomNode.tsx
@@ -1,30 +1,31 @@
-import React, { memo } from 'react';
+//import React, { memo } from 'react';
 import { Handle, Position } from 'reactflow';
 import type { NodeProps } from '@reactflow/core/dist/esm/types';
 
-export const CustomNodeBlock = ({ data }) => {
+export const CustomNodeBlock = ({ data }: {data: NodeProps}) => {
+    //TODO: is the type NodeProps?
     return (
         <>
-          <Handle
+        <Handle
             type="target"
             position={Position.Top}
             style={{ bottom: 10, background: '#555' }}
             onConnect={(params) => console.log('handle onConnect', params)}
-          />
-          <div style={{ textAlign: 'center', padding: '10px', borderRadius: '10px', backgroundColor: data.color }}>
+        />
+        <div style={{ textAlign: 'center', padding: '10px', borderRadius: '10px', backgroundColor: data.color }}>
             <div>{data.instance_of}</div>
             <div style={{fontWeight: 'bold'}}>{data.title}</div>
-          </div>
-          <Handle
+        </div>
+        <Handle
             type="source"
             position={Position.Bottom}
             style={{ top: 10, background: '#555' }}
-          />
+        />
         </>
     )
 };
 
-export const CircularNodeComponent = ({ data }) => {
+export const CircularNodeComponent = ({ data }: {data: NodeProps}) => {
     return (
         <>
         <Handle
