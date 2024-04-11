64f0f5717bfda808a9695fa607c1b1e514062a62
Timot05 <timothee.peter@mac.com>
2024-04-11 13:53:07 -0700

    add the custom edge

--- visualizer/src/CustomEdge.tsx
+++ visualizer/src/CustomEdge.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import {
-  BaseEdge,
-  EdgeLabelRenderer,
-  EdgeProps,
-  getBezierPath,
-  useReactFlow,
-} from 'reactflow';
+    BaseEdge,
+    EdgeLabelRenderer,
+    EdgeProps,
+    getBezierPath,
+    //useReactFlow,
+    } from 'reactflow';
 
 import './edge_style.css';
 
@@ -14,12 +14,12 @@ function EdgeLabel({ transform, label, isClicked }: { transform: string; label: 
     return (
       <div
         style={{
-          position: 'absolute',
-          background: 'transparent',
-          padding: 10,
-          fontSize: 12,
-          fontWeight: 700,
-          transform,
+            position: 'absolute',
+            background: 'transparent',
+            padding: 10,
+            fontSize: 12,
+            fontWeight: 700,
+            transform,
         }}
         className="nodrag nopan edge-label"
       >
@@ -40,56 +40,56 @@ export default function CustomEdge({
   markerEnd,
   data,
 }: EdgeProps) {
-  const { setEdges } = useReactFlow();
-  const [isClicked, setIsClicked] = useState(false);
+    //const { setEdges } = useReactFlow();
+    const [isClicked, setIsClicked] = useState(false);
 
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  }
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+    }
 
-  const [edgePath, labelX, labelY] = getBezierPath({
-    sourceX,
-    sourceY,
-    sourcePosition,
-    targetX,
-    targetY,
-    targetPosition,
-  });
+    const [edgePath, labelX, labelY] = getBezierPath({
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition,
+    });
 
-  return (
-    <>
-      <BaseEdge path={edgePath} markerEnd={markerEnd} style={style} />
-      <div className="edge-label">
-      <EdgeLabelRenderer>
-      <EdgeLabel
-            transform={`translate(0%, 100%) translate(${Math.floor(sourceX)}px,${Math.floor(sourceY)}px)`}
-            label= {data.source}
-            isClicked={isClicked}
-          />
-          <EdgeLabel
-            transform={`translate(0%, -100%) translate(${Math.floor(targetX)}px,${Math.floor(targetY)}px)`}
-            label= {data.target}
-            isClicked={isClicked}
-          />
-          <div
-          style={{
-            position: 'absolute',
-            transform: `translate(-50%, -50%)translate(${labelX}px,${labelY}px)`,
-            padding: 10,
-            borderRadius: 5,
-            fontSize: 12,
-            fontWeight: 700,
-            pointerEvents: 'all',
-          }}
-          className="nodrag nopan"
-          onClick={handleClick}
-        >
-        <button className="edgebutton">
-          {data.instance_of}
-        </button>
+    return (
+        <>
+        <BaseEdge path={edgePath} markerEnd={markerEnd} style={style} />
+        <div className="edge-label">
+        <EdgeLabelRenderer>
+        <EdgeLabel
+                transform={`translate(0%, 100%) translate(${Math.floor(sourceX)}px,${Math.floor(sourceY)}px)`}
+                label= {data.source}
+                isClicked={isClicked}
+            />
+            <EdgeLabel
+                transform={`translate(0%, -100%) translate(${Math.floor(targetX)}px,${Math.floor(targetY)}px)`}
+                label= {data.target}
+                isClicked={isClicked}
+            />
+            <div
+            style={{
+                position: 'absolute',
+                transform: `translate(-50%, -50%)translate(${labelX}px,${labelY}px)`,
+                padding: 10,
+                borderRadius: 5,
+                fontSize: 12,
+                fontWeight: 700,
+                pointerEvents: 'all',
+            }}
+            className="nodrag nopan"
+            onClick={handleClick}
+            >
+            <button className="edgebutton">
+            {data.instance_of}
+            </button>
+            </div>
+        </EdgeLabelRenderer>
         </div>
-      </EdgeLabelRenderer>
-      </div>
-    </>
-  );
+        </>
+    );
 }
